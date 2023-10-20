9ad488e0ed248bf585c1e7bfd803218aa30b71cc
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-20 09:36:52 +0000

    modified:   Queries/03-02.sql

--- Queries/03-02.sql
+++ Queries/03-02.sql
@@ -15,4 +15,49 @@ GROUP BY Month,s.employeeId;
 
 --using CASE statement
 
-
+SELECT
+  s.employeeId,
+  e.firstName,
+  e.lastName,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '01'
+    THEN salesAmount END) JanuarySales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '02'
+    THEN salesAmount END) febSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '03'
+    THEN salesAmount END) MarchSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '04'
+    THEN salesAmount END) AprilSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '05'
+    THEN salesAmount END) MaySales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '06'
+    THEN salesAmount END) JuneSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '07'
+    THEN salesAmount END) JulySales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '08'
+    THEN salesAmount END) AugustSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '09'
+    THEN salesAmount END) SeptemberSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '10'
+    THEN salesAmount END) OctoberSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '11'
+    THEN salesAmount END) NovemberSales,
+  sum(CASE 
+    WHEN strftime('%m',soldDate) = '12'
+    THEN salesAmount END) DecemberSales
+FROM sales s 
+INNER JOIN employee e 
+ON s.employeeId = e.employeeId
+WHERE strftime('%Y',soldDate) = '2021'
+GROUP BY s.employeeId
+ORDER BY s.employeeId
