5134892c9aa07fddd167f162f7bec5d090d47cd2
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-20 10:45:25 +0000

    modified:   Queries/03-03.sql

--- Queries/03-03.sql
+++ Queries/03-03.sql
@@ -1,1 +1,16 @@
 --Find the sales of cars that are electric by using a subquery
+
+SELECT
+  sum(s.salesAmount) totalsales,
+  count(s.salesId) numberofSales,
+  i.modelId ModelID
+  --model.model CarModel
+FROM sales s 
+INNER JOIN inventory i 
+ON s.inventoryId = i.inventoryId
+WHERE i.modelId IN (
+  SELECT modelId
+  FROM model m
+  WHERE EngineType = 'Electric')
+GROUP BY i.modelId
+ORDER BY totalsales DESC
