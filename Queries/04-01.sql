412e614acf7779b626352050b90f4da1130173d2
nipjay <147911486+nipjay@users.noreply.github.com>
2023-10-20 12:19:27 +0000

    modified:   Queries/04-01.sql

--- Queries/04-01.sql
+++ Queries/04-01.sql
@@ -24,3 +24,21 @@ GROUP BY cte.modelId,cte.employeeId
 ORDER BY e.firstName ASC,NumberofSales DESC
 
   
+--Another approach
+
+SELECT
+  e.firstName,
+  e.lastName,
+  m.model,
+  count(s.salesId) NumberofSales,
+  rank() OVER (PARTITION BY e.employeeId ORDER BY count(s.salesId) DESC) Rank
+FROM
+  employee e
+INNER JOIN sales s 
+ON s.employeeId = e.employeeId
+INNER JOIN inventory i 
+ON i.inventoryId = s.inventoryId
+INNER JOIN model m 
+ON m.modelId = i.modelId
+GROUP BY e.firstName, e.lastName, m.model
+ORDER BY e.firstName
